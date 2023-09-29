90ecca811c3759be00d2516393980302db9f43f0
hermannm <hermann.morkrid@gmail.com>
2023-09-29 13:25:34 +0200

    swap sync.Cond for slice of channels to enable use of context

--- condqueue.go
+++ condqueue.go
@@ -1,28 +1,50 @@
 package condqueue
 
 import (
+	"context"
 	"slices"
 	"sync"
 )
 
 type CondQueue[T any] struct {
-	items []T
-	cond  sync.Cond
+	items   []T
+	lock    sync.Mutex
+	waiters []chan wake
 }
+
+type wake struct{}
 
 func New[T any]() *CondQueue[T] {
-	return &CondQueue[T]{items: nil, cond: sync.Cond{L: &sync.Mutex{}}}
+	return &CondQueue[T]{items: nil, lock: sync.Mutex{}, waiters: nil}
 }
 
-func (queue *CondQueue[T]) AddItem(item T) {
-	queue.cond.L.Lock()
+func (queue *CondQueue[T]) AddItem(ctx context.Context, item T) (cancelErr error) {
+	queue.lock.Lock()
+
 	queue.items = append(queue.items, item)
-	queue.cond.L.Unlock()
-	queue.cond.Broadcast()
+	for _, waiter := range queue.waiters {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case waiter <- wake{}:
+		default:
+			// If channel is full: waiter is already awoken, do nothing
+		}
+	}
+
+	queue.lock.Unlock()
+	return nil
 }
 
-func (queue *CondQueue[T]) AwaitMatchingItem(isMatch func(T) bool) (match T) {
-	queue.cond.L.Lock()
+func (queue *CondQueue[T]) AwaitMatchingItem(
+	ctx context.Context,
+	isMatch func(T) bool,
+) (match T, cancelErr error) {
+	waiter := make(chan wake, 1)
+
+	queue.lock.Lock()
+	queue.waiters = append(queue.waiters, waiter)
+
 	for {
 		// Iterates in reverse, to get the newest item first
 		for i := len(queue.items) - 1; i >= 0; i-- {
@@ -30,11 +52,34 @@ func (queue *CondQueue[T]) AwaitMatchingItem(isMatch func(T) bool) (match T) {
 
 			if isMatch(item) {
 				queue.items = slices.Delete(queue.items, i, i+1)
-				queue.cond.L.Unlock()
-				return item
+				queue.removeWaiter(waiter)
+				queue.lock.Unlock()
+				return item, nil
 			}
 		}
 
-		queue.cond.Wait()
+		queue.lock.Unlock()
+
+		select {
+		case <-ctx.Done():
+			queue.lock.Lock()
+			queue.removeWaiter(waiter)
+			queue.lock.Unlock()
+			return match, ctx.Err()
+		case <-waiter:
+			queue.lock.Lock()
+		}
 	}
+}
+
+func (queue *CondQueue[T]) removeWaiter(waiter chan wake) {
+	remainingWaiters := make([]chan wake, 0, cap(queue.waiters))
+
+	for _, previousWaiter := range queue.waiters {
+		if previousWaiter != waiter {
+			remainingWaiters = append(remainingWaiters, previousWaiter)
+		}
+	}
+
+	queue.waiters = remainingWaiters
 }
