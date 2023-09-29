199da4bdfcffac1408e599681e8fb900a3f612ee
hermannm <hermann.morkrid@gmail.com>
2023-09-29 14:23:06 +0200

    add documentation

--- condqueue.go
+++ condqueue.go
@@ -1,3 +1,5 @@
+// Package condqueue provides a concurrent queue, on which consumers can wait for an item meeting a
+// given condition, and producers can add items to wake consumers.
 package condqueue
 
 import (
@@ -5,6 +7,11 @@ import (
 	"slices"
 )
 
+// CondQueue is a concurrent queue of items of type T. Consumer goroutines can call
+// AwaitMatchingItem to wait for an item matching a given condition to arrive in the queue. Producer
+// goroutines can call AddItem, which wakes any waiting consumers.
+//
+// A CondQueue must be initialized with condqueue.New(), and must never be dereferenced.
 type CondQueue[T any] struct {
 	items   []T
 	lock    chan lock
@@ -18,6 +25,11 @@ func New[T any]() *CondQueue[T] {
 	return &CondQueue[T]{items: nil, lock: make(chan lock, 1), waiters: nil}
 }
 
+// AddItem adds the given item to the queue, and wakes all goroutines waiting on AwaitMatchingItem,
+// so they may see if the new item is a match.
+//
+// If ctx is canceled before the queue's lock is acquired, ctx.Err() is returned. If the context
+// never cancels, e.g. when using [context.Background], the error can safely be ignored.
 func (queue *CondQueue[T]) AddItem(ctx context.Context, item T) (cancelErr error) {
 	select {
 	case <-ctx.Done():
@@ -38,6 +50,16 @@ func (queue *CondQueue[T]) AddItem(ctx context.Context, item T) (cancelErr error
 	return nil
 }
 
+// AwaitMatchingItem goes through the items in the queue, and returns an item where isMatch(item)
+// returns true. If no matching item is already in the queue, it waits until a match arrives.
+//
+// If ctx is canceled before a match is found, ctx.Err() is returned. If the context never cancels,
+// e.g. when using [context.Background], the error can safely be ignored. If a matching item is
+// never received, and the context never cancels, this may halt the calling goroutine forever. It is
+// therefore advised to use [context.WithTimeout] or similar.
+//
+// If multiple goroutines calling this may match on the same item, only one of them will receive the
+// item - i.e., every call to AddItem corresponds with one returned match from AwaitMatchingItem.
 func (queue *CondQueue[T]) AwaitMatchingItem(
 	ctx context.Context,
 	isMatch func(T) bool,
